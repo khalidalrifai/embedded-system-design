ab7424c31c1f4547ad1f3153b8b72c2629741fce
Khalid Al Rifai <ing.khaledhr@gmail.com>
2018-11-12 14:11:16 +0800

    Add files via upload

---
+++ Assignment 3/Q1/Q1.ino
@@ -0,0 +1,33 @@
+unsigned char *ddrb = (unsigned char *)0x24;
+
+unsigned char *tccr1a = (unsigned char *)0x80;
+unsigned char *tccr1b = (unsigned char *)0x81;
+unsigned char *tccr1c = (unsigned char *)0x82;
+unsigned char *ocr1ah = (unsigned char *)0x88;
+unsigned char *ocr1al = (unsigned char *)0x89;
+
+void setup()
+{
+  *tccr1a = 0b10100011;   // Fast PWM & Normal Output
+  *tccr1b = 0b00011011;   // Pre-Scaler 64
+  *ddrb = 0b00000010;
+  
+  unsigned int counter = 0;
+  long value = 0; 
+  Serial.begin(9600);
+
+
+  for (;;)
+  {
+    value = ((sin((2*PI/5000*11*counter)) +1) * 128);
+    *ocr1al = value;
+    Serial.println(value);
+    counter++;
+    delay(11);    // Sampling period = (1/90.91) = 10.99989 ms which is almost 11 ms
+  }
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
---
+++ Assignment 3/Q2/Q2.ino
@@ -0,0 +1,32 @@
+unsigned char *ddrd = (unsigned char *)0x2A;
+
+unsigned char *tccr2a = (unsigned char *)0xb0;
+unsigned char *tccr2b = (unsigned char *)0xb1;
+
+void setup()
+{
+  *ddrd = 1 << 3;
+  *tccr2a = 0b00100000;         // Phase Correct PWM 0xFF & Normal Output
+  *tccr2b = 0b00001100;         // Pre-Scaler 64
+  
+  double counter;
+  Serial.begin(9600);
+
+  for (;;)
+  {
+    counter = 0;
+    
+    while(counter < 255)
+    {
+      counter+=0.85;
+      Serial.println(counter);
+      analogWrite(3, counter);  // PWM at OC2B
+      delay(11);    // Sampling period = (1/90.91) = 10.99989 ms which is almost 11 ms
+    }
+  }
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
